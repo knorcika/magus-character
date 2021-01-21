cda715ebb26361930e40866fa0c9cf794ba39ea4
Norbert Kéri <norbert.keri@redsteedstudios.com>
2021-01-21 17:41:15 +0100

    Updates

---
+++ precache-manifest.0d023f6585be26e7cd6ecdfc2342603a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e695d62498d922bd5dfb02b7d68c6585",
+    "url": "/magus-character/index.html"
+  },
+  {
+    "revision": "95590d949f222a756a5d",
+    "url": "/magus-character/static/css/main.05a6006d.chunk.css"
+  },
+  {
+    "revision": "d35d1090d68ebef671e7",
+    "url": "/magus-character/static/js/2.8f59c8e1.chunk.js"
+  },
+  {
+    "revision": "df74735b8cfd85c15b93036317d8dce6",
+    "url": "/magus-character/static/js/2.8f59c8e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95590d949f222a756a5d",
+    "url": "/magus-character/static/js/main.d47e95a4.chunk.js"
+  },
+  {
+    "revision": "7dbca55534f3ab87f6d6",
+    "url": "/magus-character/static/js/runtime-main.a3d6f39f.js"
+  },
+  {
+    "revision": "1861ad06e94d38089add4b7a80531a67",
+    "url": "/magus-character/static/media/background.1861ad06.jpg"
+  }
+]);
